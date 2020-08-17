e34ed2d3f93adfa737f71f1d40fca9b65613d14d
Evan Pesch <epesch@merit.edu>
2020-08-17 13:02:34 -0400

    general updates: index clean grid

--- static/js/nav-height-scroll.js
+++ static/js/nav-height-scroll.js
@@ -3,16 +3,23 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-  	var nav = document.getElementsByClassName("nav");
-  	nav[0].style.height = "60px";
+  	var navContainer = document.getElementsByClassName("nav-container");
+  	navContainer[0].style.height = "60px";
 
-  	var navContainer = document.getElementsByClassName("nav-container");
-  	navContainer[0].style.marginTop = "0";
+  	var navLeft = document.getElementsByClassName("nav-left");
+  	navLeft[0].style.marginTop = "0";
+
+    var navRight = document.getElementsByClassName("nav-right");
+    navRight[0].style.marginTop = "0";
+
   } else {
-  	var nav = document.getElementsByClassName("nav");
-  	nav[0].style.height = "90px";
-  	
   	var navContainer = document.getElementsByClassName("nav-container");
-  	navContainer[0].style.marginTop = "20px";
+  	navContainer[0].style.height = "90px";
+  	
+  	var navLeft = document.getElementsByClassName("nav-left");
+  	navLeft[0].style.marginTop = "20px";
+
+    var navRight = document.getElementsByClassName("nav-right");
+    navRight[0].style.marginTop = "20px";
   }
 }
